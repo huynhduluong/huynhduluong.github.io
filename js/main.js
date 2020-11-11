bbd9a30d22f62e33fae4df05e11f9e666146426d
Huynh Du Luong <huynhduluong@gmail.com>
2020-11-11 16:51:11 +0700

    finish skill section

--- js/main.js
+++ js/main.js
@@ -1,2 +1,28 @@
 /*jslint browser: true*/
 /*global $, console, window*/
+
+/*To Top*/
+var toTop = document.getElementById("scroll-up");
+
+window.addEventListener("scroll", myFunction);
+
+function myFunction() {
+  if (document.documentElement.scrollTop > 200) {
+    toTop.style.visibility = "visible";
+  } else {
+    toTop.style.visibility = "hidden";
+  }
+}
+/*End To Top*/
+
+/*Responsive Navigation*/
+$(document).ready(function(){
+    $('.toggle').click(function(){
+        $('.toggle').toggleClass('active')
+        
+        $('nav ul').toggleClass('active-menu')
+       
+        
+    })
+});
+/*End Responsive Navigation*/

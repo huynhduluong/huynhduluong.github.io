66f87f3fadfa2815eff671763165b55a4a99718b
Huynh Du Luong <huynhduluong@gmail.com>
2020-11-13 15:31:21 +0700

    portfolio

--- js/main.js
+++ js/main.js
@@ -1,6 +1,29 @@
 /*jslint browser: true*/
 /*global $, console, window*/
 
+/*Portfolio */
+
+$(document).ready(function () {
+  $(".list").click(function () {
+    const value = $(this).attr("data-filter");
+    if (value == "all") {
+      $(".product__item").show("1000");
+    } else {
+      $(".product__item")
+        .not("." + value)
+        .hide("1000");
+      $(".product__item")
+        .filter("." + value)
+        .show("1000");
+    }
+  });
+
+  $(".list").click(function () {
+    $(this).addClass("active").siblings().removeClass("active");
+  });
+});
+/*End Portfolio */
+
 /*To Top*/
 var toTop = document.getElementById("scroll-up");
 
@@ -16,13 +39,11 @@ function myFunction() {
 /*End To Top*/
 
 /*Responsive Navigation*/
-$(document).ready(function(){
-    $('.toggle').click(function(){
-        $('.toggle').toggleClass('active')
-        
-        $('nav ul').toggleClass('active-menu')
-       
-        
-    })
+$(document).ready(function () {
+  $(".toggle").click(function () {
+    $(".toggle").toggleClass("active");
+
+    $("nav ul").toggleClass("active-menu");
+  });
 });
 /*End Responsive Navigation*/
